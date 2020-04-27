0a14ab508e652849a17f63f974ef51cc31115b42
micnncim <micnncim@gmail.com>
2020-04-27 21:30:02 +0900

    Fix github token

--- .github/actions/welcome/entrypoint.sh
+++ .github/actions/welcome/entrypoint.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+token=${INPUT_GITHUB_TOKEN}
 repository=${INPUT_REPOSITORY}
 number=${INPUT_NUMBER}
 body=${INPUT_BODY}
@@ -10,7 +11,7 @@ trigger=${INPUT_TRIGGER}
 
 if [[ ${body} =~ ${trigger} ]]; then
     curl -X POST \
-        -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+        -H "Authorization: token ${token}" \
         -H "Content-Type: application/json" \
         "https://api.github.com/repos/${repository}/issues/${number}/comments" \
         -d "$(printf '{ "body": "Welcome, @%s!" }' "${actor}")"
