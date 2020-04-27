6e28b5e021d47737eb275fcecf1ad25f735f8966
micnncim <micnncim@gmail.com>
2020-04-27 20:43:11 +0900

    Add welcome action

---
+++ welcome/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+repository=${INPUT_REPOSITORY}
+number=${INPUT_NUMBER}
+body=${INPUT_BODY}
+actor=${INPUT_ACTOR}
+trigger=${INPUT_TRIGGER}
+
+if [[ ${body} =~ ${trigger} ]]; then
+    curl -X POST \
+        -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+        -H "Content-Type: application/json" \
+        "https://api.github.com/repos/${repository}/issues/${number}/comments" \
+        -d "$(printf '{ "body": "Welcome, @%s!" }' "${actor}")"
+fi
