72286fca522f963f8d468e3fcf7ca12451466c9d
micnncim <micnncim@gmail.com>
2020-05-02 00:13:57 +0900

    yarn build

--- .github/actions/action-add-labels/dist/index.js
+++ .github/actions/action-add-labels/dist/index.js
@@ -3534,26 +3534,13 @@ function run() {
 function getAndValidateArgs() {
     const args = {
         githubToken: core.getInput('github_token', { required: true }),
-        sizeXSLabel: core.getInput('size_xs_label'),
-        sizeSLabel: core.getInput('size_s_label'),
-        sizeMLabel: core.getInput('size_m_label'),
-        sizeLLabel: core.getInput('size_l_label'),
-        sizeXLLabel: core.getInput('size_xl_label'),
-        sizeXXLLabel: core.getInput('size_xxl_label'),
-        sizeSThreshold: parseInt(core.getInput('size_s_threshold')),
-        sizeMThreshold: parseInt(core.getInput('size_m_threshold')),
-        sizeLThreshold: parseInt(core.getInput('size_l_threshold')),
-        sizeXLThreshold: parseInt(core.getInput('size_xl_threshold')),
-        sizeXXLThreshold: parseInt(core.getInput('size_xxl_threshold')),
+        labels: JSON.parse(core.getInput('labels')),
+        owner: core.getInput('owner'),
+        repo: core.getInput('repo'),
+        number: parseInt(core.getInput('number')),
         dryRun: core.getInput('dry_run') === 'true'
     };
-    for (const numberInput of [
-        'size_s_threshold',
-        'size_m_threshold',
-        'size_l_threshold',
-        'size_xl_threshold',
-        'size_xxl_threshold'
-    ]) {
+    for (const numberInput of ['number']) {
         if (isNaN(parseInt(core.getInput(numberInput)))) {
             throw Error(`input ${numberInput} did not parse to a valid integer`);
         }
@@ -5060,78 +5047,12 @@ class Processor {
         }
     }
     process() {
-        const changes = Processor.getChangedLines();
-        const desiredLabel = this.determineLabel(changes);
-        const currentLabels = this.getCurrentSizeLabels();
-        core.debug(`desiredLabel=${desiredLabel}, currentLabels=${currentLabels}`);
-        this.updateSizeLabel(desiredLabel, currentLabels);
-    }
-    getCurrentSizeLabels() {
-        const payload = github.context
-            .payload;
-        return payload.pull_request.labels
-            .filter(label => [
-            this.options.sizeXSLabel,
-            this.options.sizeSLabel,
-            this.options.sizeMLabel,
-            this.options.sizeLLabel,
-            this.options.sizeXLLabel,
-            this.options.sizeXXLLabel
-        ].includes(label.name))
-            .map(label => label.name);
-    }
-    determineLabel(changes) {
-        if (changes < this.options.sizeSThreshold) {
-            return this.options.sizeXSLabel;
-        }
-        else if (changes < this.options.sizeMThreshold) {
-            return this.options.sizeSLabel;
-        }
-        else if (changes < this.options.sizeLThreshold) {
-            return this.options.sizeMLabel;
-        }
-        else if (changes < this.options.sizeXLThreshold) {
-            return this.options.sizeLLabel;
-        }
-        else if (changes < this.options.sizeXXLThreshold) {
-            return this.options.sizeXLLabel;
-        }
-        else {
-            return this.options.sizeXXLLabel;
-        }
-    }
-    updateSizeLabel(desiredLabel, currentLabels) {
-        const payload = github.context
-            .payload;
-        const owner = github.context.repo.owner;
-        const repo = github.context.repo.repo;
-        const number = payload.pull_request.number;
-        // TODO(micnncim): Make processes asynchronous.
-        for (const currentLabel of currentLabels.filter(label => label !== desiredLabel)) {
-            if (!this.options.dryRun) {
-                this.client.issues.removeLabel({
-                    owner,
-                    repo,
-                    issue_number: number,
-                    name: currentLabel
-                });
-            }
-            core.debug(`removed label ${currentLabel} in ${owner}/${repo}#${number}`);
-        }
-        if (!this.options.dryRun && !currentLabels.includes(desiredLabel)) {
-            this.client.issues.addLabels({
-                owner: github.context.repo.owner,
-                repo: github.context.repo.repo,
-                issue_number: number,
-                labels: [desiredLabel]
-            });
-            core.debug(`added label ${desiredLabel} in ${owner}/${repo}#${number}`);
-        }
-    }
-    static getChangedLines() {
-        const payload = github.context
-            .payload;
-        return payload.pull_request.additions + payload.pull_request.deletions;
+        this.client.issues.addLabels({
+            owner: this.options.owner,
+            repo: this.options.repo,
+            number: this.options.number,
+            labels: this.options.labels
+        });
     }
 }
 exports.Processor = Processor;
