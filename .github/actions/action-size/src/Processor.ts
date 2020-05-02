6833e9ca22898af6a64ad019565c62b2bb195954
micnncim <micnncim@gmail.com>
2020-05-03 06:43:50 +0900

    Add action-size

---
+++ .github/actions/action-size/src/Processor.ts
@@ -0,0 +1,88 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+import * as Webhooks from '@octokit/webhooks';
+
+export interface ProcessorOptions {
+  githubToken: string;
+
+  sizeXSLabel: string;
+  sizeSLabel: string;
+  sizeMLabel: string;
+  sizeLLabel: string;
+  sizeXLLabel: string;
+  sizeXXLLabel: string;
+
+  sizeSThreshold: number;
+  sizeMThreshold: number;
+  sizeLThreshold: number;
+  sizeXLThreshold: number;
+  sizeXXLThreshold: number;
+}
+
+/***
+ * Processor handles processing.
+ */
+export class Processor {
+  readonly client: github.GitHub;
+  readonly options: ProcessorOptions;
+
+  constructor(options: ProcessorOptions) {
+    this.options = options;
+    this.client = new github.GitHub(options.githubToken); 
+  }
+
+  process() {
+    const changes = Processor.getChangedLines();
+    const newLabel = this.determineLabel(changes);
+    const staleLabels = this.getCurrentSizeLabels();
+
+    core.debug(`newLabel=${newLabel}, staleLabels=${staleLabels}`);
+
+    core.setOutput('new_label', newLabel)
+    core.setOutput('stale_labels', staleLabels.join('\n'))
+  }
+
+  private getCurrentSizeLabels(): string[] {
+    const payload = github.context
+      .payload as Webhooks.WebhookPayloadPullRequest;
+
+    return payload.pull_request.labels
+      .filter(label =>
+        [
+          this.options.sizeXSLabel,
+          this.options.sizeSLabel,
+          this.options.sizeMLabel,
+          this.options.sizeLLabel,
+          this.options.sizeXLLabel,
+          this.options.sizeXXLLabel
+        ].includes(label.name)
+      )
+      .map(label => label.name);
+  }
+
+  private determineLabel(changes: number): string {
+    if (changes < this.options.sizeSThreshold) {
+      return this.options.sizeXSLabel;
+    }
+    if (changes < this.options.sizeMThreshold) {
+      return this.options.sizeSLabel;
+    }
+    if (changes < this.options.sizeLThreshold) {
+      return this.options.sizeMLabel;
+    }
+    if (changes < this.options.sizeXLThreshold) {
+      return this.options.sizeLLabel;
+    }
+    if (changes < this.options.sizeXXLThreshold) {
+      return this.options.sizeXLLabel;
+    }
+    return this.options.sizeXXLLabel;
+  }
+
+  private static getChangedLines(): number {
+    const payload = github.context
+      .payload as Webhooks.WebhookPayloadPullRequest;
+
+    return payload.pull_request.additions + payload.pull_request.deletions;
+  }
+}
