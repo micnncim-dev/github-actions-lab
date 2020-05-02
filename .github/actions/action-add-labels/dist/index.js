1c959b59604ac55fac99b23ec19f508963b1cfa7
micnncim <micnncim@gmail.com>
2020-05-03 05:10:49 +0900

    Add source map

--- .github/actions/action-add-labels/dist/index.js
+++ .github/actions/action-add-labels/dist/index.js
@@ -1,4 +1,4 @@
-module.exports =
+require('./sourcemap-register.js');module.exports =
 /******/ (function(modules, runtime) { // webpackBootstrap
 /******/ 	"use strict";
 /******/ 	// The module cache
@@ -25539,3 +25539,4 @@ function onceStrict (fn) {
 /***/ })
 
 /******/ });
+//# sourceMappingURL=index.js.map
