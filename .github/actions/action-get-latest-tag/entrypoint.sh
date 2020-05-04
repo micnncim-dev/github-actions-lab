0a4629ee9706257f26bbe0047bb50da033b9f9cb
micnncim <micnncim@gmail.com>
2020-05-05 01:37:45 +0900

    Add action-get-latest-tag

---
+++ .github/actions/action-get-latest-tag/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -e
+
+if [ "${INPUT_ALL_BRANCH}" = 'true' ]; then
+    echo "::set-output name=tag::$(git describe --tags "$(git rev-list --tags --max-count=1)")"
+    exit 0
+fi
+
+git switch "${INPUT_BRANCH}"
+git fetch --tags
+echo "::set-output name=tag::$(git describe --abbrev=0 --tags)"
