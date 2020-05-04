236ec248d9d6a63d5177b655529ab5057b2c5628
micnncim <micnncim@gmail.com>
2020-05-05 01:52:45 +0900

    Fix action-get-latest-tag

--- .github/actions/action-get-latest-tag/entrypoint.sh
+++ .github/actions/action-get-latest-tag/entrypoint.sh
@@ -2,12 +2,13 @@
 
 set -e
 
+git fetch --tags
+git fetch --prune --unshallow
+
 if [ "${INPUT_ALL_BRANCH}" = 'true' ]; then
     echo "::set-output name=tag::$(git describe --tags "$(git rev-list --tags --max-count=1)")"
     exit 0
 fi
 
 git switch "${INPUT_BRANCH}"
-git fetch --tags
-git fetch --prune --unshallow
 echo "::set-output name=tag::$(git describe --abbrev=0 --tags)"
