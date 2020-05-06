54d169e700d4696aae0d870bca1677106a9fcce3
micnncim <micnncim@gmail.com>
2020-05-06 22:25:41 +0900

    Fix get-merged-pull-request

--- .github/actions/action-get-merged-pull-request/entrypoint.sh
+++ .github/actions/action-get-merged-pull-request/entrypoint.sh
@@ -4,7 +4,11 @@ set -e
 
 github_token=${INPUT_GITHUB_TOKEN}
 
-curl -s \
-    -H "Authorization: token ${github_token}" \
-    "https://api.github.com/repos/${GITHUB_REPOSITORY}/pulls?state=closed&sort=updated&direction=desc" |
-    jq -r ".[] | select(.merge_commit_sha==\"${GITHUB_SHA}\")"
+pull_request=$(
+    curl -s \
+        -H "Authorization: token ${github_token}" \
+        "https://api.github.com/repos/${GITHUB_REPOSITORY}/pulls?state=closed&sort=updated&direction=desc" |
+        jq -r ".[] | select(.merge_commit_sha==\"${GITHUB_SHA}\")"
+)
+
+echo "::set-output name=pull_request::${pull_request}"
