e7975bd85c87cf8c038923e83ea145a8afff5e76
micnncim <micnncim@gmail.com>
2020-05-06 18:43:21 +0900

    Update action-release-label

--- .github/actions/action-release-label/entrypoint.sh
+++ .github/actions/action-release-label/entrypoint.sh
@@ -2,25 +2,55 @@
 
 set -e
 
-event=${INPUT_EVENT}
 prefix=${INPUT_LABEL_PREFIX}
+github_token=${INPUT_GITHUB_TOKEN}
 
-if [ "$(echo "${event}" | jq -r '.pull_request.merged')" -ne 'merged' ]; then
-    exit 0
-fi
+run() {
+    pull_request=''
 
-label=$(echo "${event}" | jq -r ".pull_request.labels[].name | select(test(\"$prefix(major|minor|patch)\"))")
+    case "${GITHUB_EVENT_NAME}" in
+    'push')
+        pull_request="$(fetch_merged_pull_request)"
+        ;;
+    'pull_request')
+        pull_request="$(jq '.pull_request' "${GITHUB_EVENT_PATH}")"
+        ;;
+    *)
+        echo "::debug:: event ${GITHUB_EVENT_NAME} not supported"
+        ;;
+    esac
 
-if [ -z "${label}" ]; then
-    echo "::debug:: no release label"
-    exit 0
-fi
+    if [ -z "${pull_request}" ]; then
+        echo "::error:: failed to get pull request event"
+    fi
 
-if [ "$(echo "${label}" | wc -l)" -ne 1 ]; then
-    echo "::error:: multiple release labels not allowed: labels=$(echo "${label}" | tr '\n' ',')"
-    exit 1
-fi
+    label=$(echo "${pull_request}" | jq -r ".labels[].name | select(test(\"$prefix(major|minor|patch)\"))")
 
-level=${label#"${prefix}"} # e.g.) 'release/major' => 'major'
+    if [ -z "${label}" ]; then
+        echo "::debug:: no release label"
+        exit 0
+    fi
 
-echo "::set-output name=level::${level}"
+    if [ "$(echo "${label}" | wc -l)" -ne 1 ]; then
+        echo "::error:: multiple release labels not allowed: labels=$(echo "${label}" | tr '\n' ',')"
+        exit 1
+    fi
+
+    level=${label#"${prefix}"} # e.g.) 'release/major' => 'major'
+
+    echo "::set-output name=level::${level}"
+}
+
+fetch_merged_pull_request() {
+    if [ -z "${github_token}" ]; then
+        echo "::error:: github token is required for push event"
+        exit 1
+    fi
+
+    curl -s \
+        -H "Authorization: token ${github_token}" \
+        "https://api.github.com/repos/${GITHUB_REPOSITORY}/pulls?state=closed&sort=updated&direction=desc" |
+        jq -r ".[] | select(.merge_commit_sha==\"${GITHUB_SHA}\")"
+}
+
+run
