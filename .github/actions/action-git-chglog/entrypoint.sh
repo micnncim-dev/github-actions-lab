230d7708a072573443bc419e3ad70d1a2a6e8eec
micnncim <micnncim@gmail.com>
2020-07-28 19:41:37 +0900

    Update git-chglog action

--- .github/actions/action-git-chglog/entrypoint.sh
+++ .github/actions/action-git-chglog/entrypoint.sh
@@ -9,4 +9,11 @@ fi
 
 readonly tag=${INPUT_REF#refs/tags/}
 release_note=$(git-chglog "${tag}")
+
+# Escape string because GitHub Actions currently doesn't escape strings so we
+# can't use multi-line strings without such substitution.
+release_note=$(echo "${release_note}" | sed -e 's/%/%25/g')
+release_note=$(echo "${release_note}" | sed -e 's/$\\n/%0A/g')
+release_note=$(echo "${release_note}" | sed -e 's/$\\r/%0D/g')
+
 echo "::set-output name=release_note::${release_note}"
